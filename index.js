cf8ac7efa0b209ec9aaf845b5e3e847f2022ec53
CDog5 <callum.sheppard78@btinternet.com>
2021-02-28 09:50:12 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const cardwrapper = document.querySelector('.card-wrapper');
 const cards = new Array(7);
 const startbtn = document.querySelector('#start');
+const scrtxt = document.querySelector('#score');
+let score;
 let before;
+function isCorrect(id,guess){
+  const thiscard = document.getElementById(id);
+  thiscard.innerText = cards[id].no;
+  if((thiscard.innerText > before && guess == "upper") || (thiscard.innerText  < before && guess == "lower")){
+    thiscard.style.backgroundColor = "#1FD537";
+    score+=1;
+    scrtxt.innerText = score;
+  }
+  else{
+    thiscard.style.backgroundColor = "#E40010";
+  }
+  before = parseInt(thiscard.innerText);
+}
 class StartCard {
-  constructor(id) {
-    this.id = id;
+  constructor() {
     this.value = Math.floor((Math.random() * 10) + 1);
     before = this.value;
-    this.colour = "#BEBEBE";
     this.element = document.createElement("div");
     this.element.classList.add('card');
-    this.element.style.backgroundColor = this.colour;
+    this.element.style.backgroundColor = "#BEBEBE";
     this.element.innerText = this.value;
-    this.element.id = id;
+;
     cardwrapper.appendChild(this.element);
   }
 }
@@ -35,42 +48,29 @@ class Card {
       this.element.appendChild(this.higher);
       this.higher.id = id;
       this.lower.id = id;
-      this.lower.onclick = this.lowerClick;
-      this.higher.onclick = this.upperClick;
+      this.lower.onclick = function(){isCorrect(this.id,"lower");};
+      this.higher.onclick = function(){isCorrect(this.id,"upper");};
       cardwrapper.appendChild(this.element);
     }
-    lowerClick(){
-      const thiscard = document.getElementById(this.id)
-      thiscard.innerText = cards[this.id].no;
-      if(before > parseInt(thiscard.innerText)){
-        thiscard.style.backgroundColor = "#1FD537";
-      }
-      else{
-        thiscard.style.backgroundColor = "#E40010";
-      }
-      before = parseInt(thiscard.innerText);
-    }
-    upperClick(){
-      const thiscard = document.getElementById(this.id)
-      thiscard.innerText = cards[this.id].no;
-      if(before < parseInt(thiscard.innerText)){
-        thiscard.style.backgroundColor = "#1FD537";
-      }
-      else{
-        thiscard.style.backgroundColor = "#E40010";
-      }
-    }
-    before = parseInt(thiscard.innerText);
   }
 function start() {
+  score = 0;
+  scrtxt.innerText = score;
   cardwrapper.innerHTML = "";
   for(var i = 0; i < 7; i++){
     if(i == 0){
-      cards[i] = new StartCard(i);
+      cards[i] = new StartCard();
     }
     else{
       cards[i] = new Card(i);
     }
   }
 }
+function revealAll(){
+    for(var i = 1; i < 7; i++){
+      if(cards[i]){
+        cards[i].element.innerText = cards[i].no;
+      }
+    }
+  }
 startbtn.onclick = start;
